048ad47fc34ee4278909df7ff493f32e01cd3b77
Naitik Sharma <147588654+theshion@users.noreply.github.com>
2024-04-29 13:28:48 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -51,7 +51,7 @@ class Bot(Client):
         except Exception as e:
             self.LOGGER(__name__).warning(e)
             self.LOGGER(__name__).warning(f"Make Sure bot is Admin in DB Channel, and Double check the CHANNEL_ID Value, Current Value {CHANNEL_ID}")
-            self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/CodeXBotzSupport for support")
+            self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/EonixCore for support")
             sys.exit()
 
         self.set_parse_mode(ParseMode.HTML)
