52a85f4f99af09645b4b29e1976f35a35ca9120a
Naitik Sharma <147588654+theshion@users.noreply.github.com>
2024-04-29 16:41:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,13 @@
+#(©)CodeXBotz
+
+
+
+
 import os
 import logging
 from logging.handlers import RotatingFileHandler
+
+
 
 #Bot token @Botfather
 TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
@@ -25,8 +32,7 @@ DB_URI = os.environ.get("DATABASE_URL", "")
 DB_NAME = os.environ.get("DATABASE_NAME", "filesharexbot")
 
 #force sub channel id, if you want enable force sub
-# FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
-FORCE_SUB_CHANNEL = os.environ.get("FORCE_SUB_CHANNEL", "-1001607555631, -1002099926662, -1001592833168")
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
