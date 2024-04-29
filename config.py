fc4283a6025b9b6369d1cf7e7f0ea9f4d3828a62
Naitik Sharma <147588654+theshion@users.noreply.github.com>
2024-04-29 16:11:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -26,7 +26,7 @@ DB_NAME = os.environ.get("DATABASE_NAME", "filesharexbot")
 
 #force sub channel id, if you want enable force sub
 # FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
-FORCE_SUB_CHANNEL = os.environ.get("FORCE_SUB_CHANNEL", "0")
+FORCE_SUB_CHANNEL = os.environ.get("FORCE_SUB_CHANNEL", "-1001607555631, -1002099926662, -1001592833168")
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
