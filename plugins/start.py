d903a5cd889eecb04d1a620d6394c6350b1904ba
Naitik Sharma <147588654+theshion@users.noreply.github.com>
2024-04-29 14:00:46 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -1,8 +1,3 @@
-#(©)CodeXBotz
-
-
-
-
 import os
 import asyncio
 from pyrogram import Client, filters, __version__
@@ -14,9 +9,6 @@ from bot import Bot
 from config import ADMINS, FORCE_MSG, START_MSG, CUSTOM_CAPTION, DISABLE_CHANNEL_BUTTON, PROTECT_CONTENT
 from helper_func import subscribed, encode, decode, get_messages
 from database.database import add_user, del_user, full_userbase, present_user
-
-
-
 
 @Bot.on_message(filters.command('start') & filters.private & subscribed)
 async def start_command(client: Client, message: Message):
@@ -123,8 +115,14 @@ async def not_joined(client: Client, message: Message):
     buttons = [
         [
             InlineKeyboardButton(
-                "Join Channel",
-                url = client.invitelink)
+                "Join Channel 1",
+                url = "https://t.me/EonixCore"),
+            InlineKeyboardButton(
+                "Join Channel 2",
+                url = "https://t.me/BontenCriminals"),
+            InlineKeyboardButton(
+                "Join Channel 3",
+                url = "https://t.me/AC_Anime_Group")
         ]
     ]
     try:
